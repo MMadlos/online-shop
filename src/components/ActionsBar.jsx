208f85ebc457b3a062452e1cdefefd160fb868ad
michael <michaelmadlos@hotmail.com>
2023-10-19 11:54:28 +0200

    Add styles to messages when a search is not found or when a sort option is selected

--- src/components/ActionsBar.jsx
+++ src/components/ActionsBar.jsx
@@ -7,25 +7,38 @@ function ActionsBar() {
 	const { item, group } = sort
 
 	return (
-		<div className="action-bar">
-			<form action="search">
-				<i className="fa-solid fa-magnifying-glass"></i>
-				<input
-					name="search"
-					type="search"
-					id="search"
-					placeholder="Backpack"
-					onChange={handleChangeSearch}
-				/>
-			</form>
-			{item !== "Default" && (
-				<p>
-					Sorted: {group} - {item}
-				</p>
-			)}
-			{!isProductFound && <p id="search-not-found"> Product not found with your search</p>}
-			<Dropdown />
-		</div>
+		<>
+			<div className="action-bar">
+				<form action="search">
+					<i className="fa-solid fa-magnifying-glass"></i>
+					<input
+						name="search"
+						type="search"
+						id="search"
+						placeholder="Backpack"
+						onChange={handleChangeSearch}
+					/>
+				</form>
+
+				<Dropdown />
+			</div>
+			<div className="action-messages">
+				{!isProductFound && <p className="search-not-found"> Product not found with your search</p>}
+
+				{item !== "Default" && (
+					<div className="chip-sort">
+						<p>Sorted:</p>
+
+						<div className="sort-selected">
+							<p>
+								{group} - {item}
+							</p>
+							<i className="fa-solid fa-circle-xmark"></i>
+						</div>
+					</div>
+				)}
+			</div>
+		</>
 	)
 }
 
