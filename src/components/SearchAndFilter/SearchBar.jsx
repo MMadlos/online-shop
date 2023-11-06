6e69fca8fe8e8338017a0aa6a54e93bd2b3da96b
michael <michaelmadlos@hotmail.com>
2023-11-06 16:51:17 +0100

    Add message when searching for a product can't find any and add style

--- src/components/SearchAndFilter/SearchBar.jsx
+++ src/components/SearchAndFilter/SearchBar.jsx
@@ -1,15 +1,27 @@
+import { useContext } from "react"
+import { SearchAndSortContext } from "../../App"
+
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons"
 
 function SearchBar() {
+	const { handleChangeSearch, isProductFound } = useContext(SearchAndSortContext)
+
 	return (
-		<div className="search-container">
-			<FontAwesomeIcon icon={faMagnifyingGlass} />
-			<input
-				type="search"
-				placeholder="Casual jacket"
-			/>
-		</div>
+		<>
+			<div className="search-container">
+				<form action="search">
+					<FontAwesomeIcon icon={faMagnifyingGlass} />
+					<input
+						name="search"
+						type="search"
+						placeholder="Casual jacket"
+						onChange={handleChangeSearch}
+					/>
+				</form>
+			</div>
+			{!isProductFound && <p className="search-not-found"> Product not found with your search</p>}
+		</>
 	)
 }
 
