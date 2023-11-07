0263d02200f7cebad509e8e12e57fe069a3b14b3
michael <michaelmadlos@hotmail.com>
2023-11-07 15:31:13 +0100

    Remove commented code

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -1,15 +1,12 @@
-// import ActionsBar from "./ActionsBar"
 import ProductList from "./ProductList"
 import SearchAndSort from "./SearchAndSort/SearchAndSort"
 
 function HomePage() {
 	return (
-		<>
-			<div className="home">
-				<SearchAndSort />
-				<ProductList />
-			</div>
-		</>
+		<div className="home">
+			<SearchAndSort />
+			<ProductList />
+		</div>
 	)
 }
 
