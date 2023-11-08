2c1588c01c7e26f1a427842fec13e52ef104aef7
michael <michaelmadlos@hotmail.com>
2023-11-08 13:54:22 +0100

    Add jsx structure for new CartProduct component

--- src/components/CartPage/CartProduct.jsx
+++ src/components/CartPage/CartProduct.jsx
@@ -7,6 +7,24 @@ function CartProduct({ product }) {
 
 	return (
 		<Link to={`/product/${id}`}>
+			<div className="cart-product">
+				<img
+					src="url"
+					alt=""
+				/>
+				<div className="cart-info-container">
+					<div className="cart-header-container">
+						<p className="name">{name}</p>
+						<i className="fa-regular fa-trash-can"></i>
+					</div>
+					<div className="product-quantity-container">
+						<p>{`Quantity: ${quantity}`}</p>
+						<i className="fa-solid fa-pencil"></i>
+					</div>
+					<p className="price">$ {price}</p>
+				</div>
+			</div>
+
 			<div
 				key={id}
 				className="product-card cart"
