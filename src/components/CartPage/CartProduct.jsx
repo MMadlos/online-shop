92e4f488dbf9b20f00d7dad813c94d8d08d58e32
michael <michaelmadlos@hotmail.com>
2023-11-08 14:04:31 +0100

    Add styles to CartProduct for mobile and remove previous code

--- src/components/CartPage/CartProduct.jsx
+++ src/components/CartPage/CartProduct.jsx
@@ -1,16 +1,15 @@
-import Counter from "../Atoms/Counter"
-import RemoveCartButton from "../Atoms/RemoveCartButton"
 import { Link } from "react-router-dom"
 
 function CartProduct({ product }) {
-	const { id, name, price, url, quantity, description } = product
+	const { id, name, price, url, quantity } = product
 
 	return (
 		<Link to={`/product/${id}`}>
 			<div className="cart-product">
 				<img
-					src="url"
+					src={url}
 					alt=""
+					className="cart-product-image"
 				/>
 				<div className="cart-info-container">
 					<div className="cart-header-container">
@@ -22,34 +21,6 @@ function CartProduct({ product }) {
 						<i className="fa-solid fa-pencil"></i>
 					</div>
 					<p className="price">$ {price}</p>
-				</div>
-			</div>
-
-			<div
-				key={id}
-				className="product-card cart"
-				data-product-id={id}>
-				<div className="product-info">
-					<div className="product-info-details-container">
-						<img
-							src={url}
-							alt=""
-						/>
-
-						<div className="produt-info-name-price-container">
-							<p className="name">
-								<strong>{name}</strong>
-							</p>
-							<p className="description">{description}</p>
-							<p className="price">$ {price}</p>
-						</div>
-					</div>
-
-					<Counter
-						context="cart"
-						currentQuantity={quantity}
-					/>
-					<RemoveCartButton />
 				</div>
 			</div>
 		</Link>
