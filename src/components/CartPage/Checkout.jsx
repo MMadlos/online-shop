95827fb2c48d70563130b655a7e864f4c996d6c8
michael <michaelmadlos@hotmail.com>
2023-11-09 21:40:16 +0100

    Create CheckoutPage; Add styles for Mobile

--- src/components/CartPage/Checkout.jsx
+++ src/components/CartPage/Checkout.jsx
@@ -2,42 +2,47 @@ import Chip from "../Elements/Chip"
 import shippingIMG from "../../assets/free-shipping-tiny.png"
 import Button from "../Elements/Button"
 
+import CheckoutPage from "./CheckoutPage"
+
 function Checkout({ totalToPay, totalItems }) {
 	return (
-		<div className="checkout-container">
-			<img
-				src={shippingIMG}
-				alt=""
-				className="shipping-image"
-			/>
-			<div className="cost-list">
-				<div className="subtotal">
-					<p>Number of items</p>
-					<p>{totalItems}</p>
+		<>
+			<div className="checkout-container">
+				<img
+					src={shippingIMG}
+					alt=""
+					className="shipping-image"
+				/>
+				<div className="cost-list">
+					<div className="subtotal">
+						<p>Number of items</p>
+						<p>{totalItems}</p>
+					</div>
+					<div className="subtotal">
+						<p>Subtotal</p>
+						<p>$ {totalToPay}</p>
+					</div>
+					<div className="divider"></div>
+					<div className="shipping-cost">
+						<p>Shipping cost</p>
+						<Chip
+							text="FREE"
+							type="accent"
+						/>
+					</div>
+					<div className="total-amount">
+						<p>TOTAL</p>
+						<p>$ {totalToPay}</p>
+					</div>
+					<p className="taxes">(Taxes included)</p>
 				</div>
-				<div className="subtotal">
-					<p>Subtotal</p>
-					<p>$ {totalToPay}</p>
-				</div>
-				<div className="divider"></div>
-				<div className="shipping-cost">
-					<p>Shipping cost</p>
-					<Chip
-						text="FREE"
-						type="accent"
-					/>
-				</div>
-				<div className="total-amount">
-					<p>TOTAL</p>
-					<p>$ {totalToPay}</p>
-				</div>
-				<p className="taxes">(Taxes included)</p>
+				<Button
+					text="Checkout"
+					btnID="checkout"
+				/>
 			</div>
-			<Button
-				text="Checkout"
-				btnID="checkout"
-			/>
-		</div>
+			<CheckoutPage />
+		</>
 	)
 }
 
---
+++ src/components/CartPage/CheckoutPage.jsx
@@ -0,0 +1,35 @@
+import Chip from "../Elements/Chip"
+import CelebrationIMG from "../../assets/celebration.svg"
+import Button from "../Elements/Button"
+
+function CheckoutPage() {
+	return (
+		<div id="checkout-page">
+			<Chip
+				text="Checkout completed!"
+				type="accent"
+			/>
+			<img
+				src={CelebrationIMG}
+				alt=""
+			/>
+			<div className="checkout-text-container">
+				<h4>The payment process is out of the scope of this project.</h4>
+				<p>{`Thank you for going throught this project! :)`}</p>
+				<button>
+					<i className="fa-solid fa-arrow-left"></i>Back to the shop
+				</button>
+			</div>
+			<div className="link-container">
+				<Button
+					text="Portfolio"
+					btnID="back"
+				/>
+				<i className="fa-brands fa-github"></i>
+				<i className="fa-brands fa-linkedin-in"></i>
+			</div>
+		</div>
+	)
+}
+
+export default CheckoutPage
