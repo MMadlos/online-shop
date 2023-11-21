b42c9cd7ff323847c5fac7ae10e482869c4e170f
michael <michaelmadlos@hotmail.com>
2023-11-21 11:30:05 +0100

    Add ErrorProduct content and styles

--- src/components/Error/ErrorProduct.jsx
+++ src/components/Error/ErrorProduct.jsx
@@ -1,5 +1,19 @@
+import "./styles.css"
+
+import { useParams } from "react-router-dom"
+
 function ErrorProduct() {
-	return <h1>There is no product here</h1>
+	const { productID } = useParams()
+	return (
+		<section className="no-product-found">
+			<h1>Oh no! There is no product that matches the ID {productID}</h1>
+			<ul>
+				<li>- Check if the ID you've introduced in the URL is correct</li>
+				<li>- The product you're searching for may be discontinued or not yet on our site</li>
+				<li>- Go back to the shop. We are sure that you'll find something that you like :)</li>
+			</ul>
+		</section>
+	)
 }
 
 export default ErrorProduct
