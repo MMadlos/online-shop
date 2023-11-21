22e027c6d94280f743a20490c5b2ad720b254956
michael <michaelmadlos@hotmail.com>
2023-11-21 12:33:53 +0100

    Add error page and its styles

--- src/components/Error/ErrorPage.jsx
+++ src/components/Error/ErrorPage.jsx
@@ -1,11 +1,28 @@
+import "./styles.css"
+import errorPageSVG from "../../assets/error-page.svg"
 import { Link } from "react-router-dom"
 
 function ErrorPage() {
 	return (
-		<>
-			<h1>PENDING ERROR PAGE</h1>
-			<Link to="/">Click here to back to the main page</Link>
-		</>
+		<section className="error-page">
+			<img
+				src={errorPageSVG}
+				alt=""
+			/>
+			<div className="content-container">
+				<div className="title-error">
+					<p>404</p>
+					<h1>Something's missing.</h1>
+				</div>
+				<p className="explanation-error">This page is missing or you assembled the link incorrectly</p>
+
+				<Link to="/">
+					<button>
+						<i className="fa-solid fa-arrow-left" /> Go back to home
+					</button>
+				</Link>
+			</div>
+		</section>
 	)
 }
 
